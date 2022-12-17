6a823f5ac19b3caf887e447202f43764b85b3d15
loveohamtom3 <lovephamtom@icloud.com>
2022-12-17 21:46:01 +0900

    port

--- myapp/views.py
+++ myapp/views.py
@@ -52,7 +52,7 @@ class SignUp(CreateView):
     form_class = SignUpForm
     template_name = 'myapp/signup.html'
 
-    def post(self, request, *args, **kwargs):
+    def post(self, request):
         form = self.form_class(data=request.POST)
         if form.is_valid():
             form.save()
@@ -64,9 +64,28 @@ class SignUp(CreateView):
         return render(request, 'myapp/signup.html', {'form': form})
 
 
-class Login(LoginView):
-    form_class = LoginForm
-    template_name = 'myapp/login.html'
+def LoginView(request):
+    if request.method == 'POST':
+     next = request.POST.get('next')
+     form = LoginForm(request, data=request.POST)
+
+    if form.is_valid():
+       user = form.get_user()
+       if user:
+        login(request, user)
+        if next == 'None':
+          return redirect(to='myapp/signup.html')
+        else:
+          return redirect(to=next)
+    else:
+        form = LoginForm()
+        next = request.GET.get('next')
+
+    param = {
+        'form': form,
+        'next': next
+    }
+    return render(request, 'myapp/login.html', param)
 
 
 class Logout(LogoutView):
